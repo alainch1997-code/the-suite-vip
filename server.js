4f998c05155f0e066a3eef7bb57b984e3b4883bb
alainch1997-code <alain.ch1997@gmail.com>
2026-08-23 15:38:40 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "index.html");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Unable to load The Suite app.");
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8"
+    });
+
+    res.end(data);
+  });
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`The Suite is running on port ${PORT}`);
+});
